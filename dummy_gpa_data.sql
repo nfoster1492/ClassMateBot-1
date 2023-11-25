af09f507f349cbadd538b74d297868ca92ee9c4e
Jacob Gerlach <jwgerlach00@gmail.com>
2023-11-25 01:35:36 +0000

    Populating other relations in dummy data to avoid having to input from CLI

--- dummy_gpa_data.sql
+++ dummy_gpa_data.sql
@@ -18,3 +18,21 @@ VALUES
     ('C', 70, 79),
     ('D', 60, 69),
     ('F', 0, 59);
+
+INSERT INTO grade_categories
+VALUES
+    (1, '1143956280222957578', 'project', 'midterm1', 0.200),
+    (3, '1143956280222957578', 'test', 'project1', 0.700),
+    (4, '1143956280222957578', 'hw', 'hw1', 0.100);
+
+INSERT INTO assignments
+VALUES
+    (1, '1143956280222957578', 3, 'midterm1', 95),
+    (2, '1143956280222957578', 1, 'project1', 100),
+    (4, '1143956280222957578', 4, 'hw1', 100);
+
+INSERT INTO grades
+VALUES
+    ('1143956280222957578', 'cornonjacob', 1, 92),
+    ('1143956280222957578', 'cornonjacob', 2, 70),
+    ('1143956280222957578', 'cornonjacob', 4, 90);
