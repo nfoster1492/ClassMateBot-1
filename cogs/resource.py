29745a999f59b71ca1e67f292f2c090e4afea314
root <ukpaulbd@gmail.com>
2023-11-27 14:30:41 +0000

    done

--- cogs/resource.py
+++ cogs/resource.py
@@ -30,22 +30,9 @@ class Resource(commands.Cog):
     @commands.command(
         name="addResource",
         help="To use the addresource command, do: $addresource topic_name resource_link <Num> \n \
-        ( For example: $addresource Ethical_Software_Engineering  )"
+        ( For example: $addResource Ethical_Software_Engineering, https://github.com/txt/se23/blob/main/docs/ethics.md  )"
     )
     async def addResource(self, ctx, topic, resource_link):
-        if topic is None :
-            await ctx.send("To add resource, You must provide the topic name")
-            return
-        if resource_link is None :
-            await ctx.send("To add resource, You must provide the resource link")
-            return
-        create_table_query = """
-            CREATE TABLE resources (
-                guild_id        BIGINT NOT NULL,
-                topic_name      VARCHAR NOT NULL,
-                resource_link   VARCHAR NOT NULL
-            );
-            """
         db.query(
             "INSERT INTO resources (guild_id, topic_name, resource_link) VALUES (%s, %s, %s)",
             (ctx.guild.id,  topic, resource_link),
@@ -54,10 +41,10 @@ class Resource(commands.Cog):
         return
     @addResource.error
     async def addResource_error(self, ctx, error):
-        """Error handling for group command"""
+        """Error handling for resource add"""
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(
-                "To use the group command, do: $group <Num> \n ( For example: $group 0 )"
+            await ctx.send("To use the addresource command, do: $addresource topic_name resource_link <Num> \n \
+            ( For example: $addresource Ethical_Software_Engineering, https://github.com/txt/se23/blob/main/docs/ethics.md  )"
             )
         else:
             await ctx.author.send(error)
